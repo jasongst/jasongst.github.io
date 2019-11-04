eadf440795144c80eece6798ecc3d05f7ca6738e
Jason Guestin <jasonguestin@gmail.com>
2019-11-04 20:21:28 +0100

    Ajout typewriter

--- src/js/script.js
+++ src/js/script.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottom_of_window > bottom_of_object  ){
 
                 $(this).animate({'opacity':'1'},500);
 
@@ -45,21 +45,27 @@ $(document).ready(function() {
         $(this).toggleClass('flip');
     });
 
-    // Affichage du texte lettre par lettre
-    function showText (target, message, index, interval) {
-        if (index < message.length) {
-            $(target).append(message[index++]);
-            setTimeout(function () { showText(target, message, index, interval); }, interval);
-        }
-    }
+    //Typewriter.js
+    var demo = document.getElementById('twriter');
 
-    //$('.banner-content').css('height','40vh');
 
-    setTimeout(function () {showText(".banner h1", "< JASON GUESTIN />", 0, 130);},800);
+    $(".banner h1").css('opacity','1');
+    $(".banner a").css('opacity','1');
+
+
     setTimeout(function(){
-        $(".banner h2").css('opacity','1');
-        $(".banner a").css('opacity','1');
-    },2500);
+        var typewriter = new Typewriter(demo, {
+            cursor: '_',
+            loop: true,
+        });
+        typewriter.typeString('Développeur full-stack')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('Étudiant en DUT Informatique')
+            .pauseFor(2500)
+            .deleteAll()
+            .start();
+    },1000);
 
 
 });
